e1b4ddefc806a5928bee9292a1b01862a95596ca
João Carvalho <joaokvalho@gmail.com>
2022-08-31 19:15:51 -0300

    feat: added internal handle error

--- core/errs/handle_error.go
+++ core/errs/handle_error.go
@@ -17,38 +17,42 @@ func New(message string) *appError {
 	}
 }
 
-func (e *appError) NotCataloged() error {
+func (e *appError) Error() string {
+	return e.message
+}
+
+func (e *appError) NotCataloged() *appError {
 	e.statusCode = http.StatusInternalServerError
 	e.Err = errors.New("Internal Error, not cataloged: " + e.message)
-	return e.Err
+	return e
 }
 
-func (e *appError) SystemError() error {
+func (e *appError) SystemError() *appError {
 	e.statusCode = http.StatusInternalServerError
 	e.Err = errors.New("System Error: " + e.message)
-	return e.Err
+	return e
 }
 
-func (e *appError) NotFound() error {
+func (e *appError) NotFound() *appError {
 	e.statusCode = http.StatusNotFound
 	e.Err = errors.New("Resource not found: " + e.message)
-	return e.Err
+	return e
 }
 
-func (e *appError) BussinesError() error {
+func (e *appError) BussinesError() *appError {
 	e.statusCode = http.StatusBadRequest
 	e.Err = errors.New("Bussines Error: " + e.message)
-	return e.Err
+	return e
 }
 
-func (e *appError) EntityInUseError() error {
+func (e *appError) EntityInUseError() *appError {
 	e.statusCode = http.StatusFailedDependency
 	e.Err = errors.New("Entity in use: " + e.message)
-	return e.Err
+	return e
 }
 
-func (e *appError) ConflictError() error {
+func (e *appError) ConflictError() *appError {
 	e.statusCode = http.StatusConflict
 	e.Err = errors.New("Conflict Error: " + e.message)
-	return e.Err
+	return e
 }
